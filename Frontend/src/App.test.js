c64ce90b7825fbf2f999272d1885ae9793d938cc
Ashutoshbirje <ashutoshbirje4@gmail.com>
2026-04-20 00:12:01 +0530

    ok

--- Frontend/src/App.test.js
+++ Frontend/src/App.test.js
@@ -1,6 +1,13 @@
 import { render } from '@testing-library/react';
 import App from './App';
 
+jest.mock('react-router-dom', () => ({
+  BrowserRouter: ({ children }) => children,
+  Routes: ({ children }) => children,
+  Route: () => null,
+  Navigate: () => null,
+}));
+
 test('app renders', () => {
   render(<App />);
 });
